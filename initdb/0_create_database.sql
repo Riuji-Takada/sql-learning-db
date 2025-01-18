0c0f373ae549d94ae4de191ac85d56f9fbc77d3f
Riuji Takada <takada.riuji@gmail.com>
2025-01-18 17:45:11 +0900

    テーブル作成SQLの更新

--- initdb/0_create_database.sql
+++ initdb/0_create_database.sql
@@ -2,4 +2,20 @@ SET CHARSET utf8;
 
 CREATE DATABASE IF NOT EXISTS `school_db`;
 GRANT ALL ON `school_db`.* TO learningUser;
+
+CREATE DATABASE IF NOT EXISTS `entry_exercises_db`;
+GRANT ALL ON `entry_exercises_db`.* TO learningUser;
+
+CREATE DATABASE IF NOT EXISTS `elementary_exercises_db`;
+GRANT ALL ON `elementary_exercises_db`.* TO learningUser;
+
+CREATE DATABASE IF NOT EXISTS `intermediate_exercises_db`;
+GRANT ALL ON `intermediate_exercises_db`.* TO learningUser;
+
+CREATE DATABASE IF NOT EXISTS `advanced_exercises_db`;
+GRANT ALL ON `advanced_exercises_db`.* TO learningUser;
+
+CREATE DATABASE IF NOT EXISTS `master_exercises_db`;
+GRANT ALL ON `master_exercises_db`.* TO learningUser;
+
 FLUSH PRIVILEGES;
